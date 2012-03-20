18afa91e7914e41b25822c84d2a9ea9fe78d18f3
dritter <ritter@3tools.de>
2012-03-21 00:57:56 +0100

    Als Author wird nun automatisch der eingeloggte Benutzer genommen...

--- src/Kcb/Bonnliga/Bundle/WebsiteBundle/Entity/Blog/Post.php
+++ src/Kcb/Bonnliga/Bundle/WebsiteBundle/Entity/Blog/Post.php
@@ -2,14 +2,14 @@
 
 namespace Kcb\Bonnliga\Bundle\WebsiteBundle\Entity\Blog;
 
-use Sylius\Bundle\BloggerBundle\Entity\Post as BasePost;
+use Sylius\Bundle\BloggerBundle\Model\SignedPost;
 use Doctrine\ORM\Mapping as ORM;
 use Kcb\Bonnliga\Bundle\WebsiteBundle\Entity\User;
 
 /**
  * @ORM\Entity
  */
-class Post extends BasePost {
+class Post extends SignedPost {
 
     /**
      * @ORM\Id
@@ -17,6 +17,42 @@ class Post extends BasePost {
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+
+    /**
+     * @ORM\Column
+     */
+    protected $title;
+
+    /**
+     * @ORM\Column
+     */
+    protected $slug;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Kcb\Bonnliga\Bundle\WebsiteBundle\Entity\User")
+     * @ORM\JoinColumn(name="user", referencedColumnName="id")
+     */
+    protected $user;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    protected $content;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    protected $published;
+
+    /**
+     * @ORM\Column(type="datetime", name="created_at")
+     */
+    protected $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime", name="updated_at")
+     */
+    protected $updatedAt;
 
     public function __construct() {
         parent::__construct();
